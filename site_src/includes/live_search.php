2c67a8b9482b38c009dad49a7880baa4e10433ae
1n5aN1aC <joshuavillwo@gmail.com>
2012-11-16 11:32:38 -0800

    Update site_src/includes/live_search.php

--- site_src/includes/live_search.php
+++ site_src/includes/live_search.php
@@ -2,7 +2,7 @@
 include 'core/init.php';
 protect_page();
 
-//this needs fixed, but I don't know the database tables enough yet to fix it.
+//this needs fixed, but I don't know the database tables enough yet to know what to query to fix it.
 @mysql_select_db("joshua_class") or die("Unable to select database");
 $result = mysql_query("SELECT * FROM SERVERS WHERE Server_Name LIKE '%" . $_GET["s"] . "%' LIMIT 0, 6");
 
@@ -11,7 +11,7 @@ echo "<center><table border='1'><tr>
 <th>Food Name</th>
 <th>ID Number</th>
 <th>Calories</th>
-<th>etc</th>
+<th></th>
 <th>etc</th>
 </tr>";
 
@@ -21,7 +21,7 @@ while($row = mysql_fetch_array($result))
   echo "<tr><td>";
   echo $row['Food_Name'] . "</td><td>" . $row['ID_Number'];
   echo "<td><a href='https://rt.oregonsdc.org/Ticket/Display.html?id=" . $row[calories] . "'>" . $row[calories] . "</a></td>";
-  echo "<td><a href='#' onclick=removesubmit(" . $row[Server_ID] . ")><img src='img/Delete_Icon.png' alt='Delete'/></a></td>";
+  echo "<td><a href='#' onclick=fooddetails(" . $row[details] . ")><img src='img/Food_Details.png' alt='Details'/></a></td>";
   echo "<td><a href='Edit.php?s=" . $row[Server_ID] . "'><img src='img/Edit_Icon.png' alt='Edit'/></a></td>";
   echo "</td></tr>";
 }
