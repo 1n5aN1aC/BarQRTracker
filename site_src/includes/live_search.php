68727100b5b89d28f5f8e24c6f18377065d4ab75
1n5aN1aC <joshuavillwo@gmail.com>
2012-11-27 15:16:16 -0800

    Update for DB Schema

--- site_src/includes/live_search.php
+++ site_src/includes/live_search.php
@@ -3,26 +3,26 @@ include 'core/init.php';
 protect_page();
 
 //this needs fixed, but I don't know the database tables enough yet to know what to query to fix it.
-@mysql_select_db("joshua_class") or die("Unable to select database");
-$result = mysql_query("SELECT * FROM SERVERS WHERE Server_Name LIKE '%" . $_GET["s"] . "%' LIMIT 0, 6");
+$result = mysql_query("SELECT * FROM Food WHERE Name LIKE '%" . $_GET["s"] . "%' LIMIT 0, 6");
 
 //Output Table openings
 echo "<center><table border='1'><tr>
 <th>Food Name</th>
-<th>ID Number</th>
+<th>Food ID</th>
 <th>Calories</th>
-<th></th>
-<th>etc</th>
+<th>Fat</th>
+<th>Add</th>
 </tr>";
 
 //Operate on the results
 while($row = mysql_fetch_array($result))
 {
-  echo "<tr><td>";
-  echo $row['Food_Name'] . "</td><td>" . $row['ID_Number'];
-  echo "<td><a href='https://rt.oregonsdc.org/Ticket/Display.html?id=" . $row[calories] . "'>" . $row[calories] . "</a></td>";
-  echo "<td><a href='#' onclick=fooddetails(" . $row[ID_Number] . ")><img src='img/Food_Details.png' alt='Details'/></a></td>";
-  echo "<td><a href='#' onclick=editfood(" . $row[ID_Number] . "')><img src='img/Edit_Icon.png' alt='Edit'/></a></td>";
+  echo "<tr>";
+  echo "<td>" . $row['Name'] . "</td>";
+  echo "<td>" . $row['IDFood']; "</td>";
+  echo "<td>" . $row['Calories'] . "</td>";
+  echo "<td>" . $row['totalFat'] . "</td>";
+  echo "<td><a href='#' onclick=addfood(" . $row[ID_Number] . "')><img src='img/Add_Icon.png' alt='Add'/></a></td>";
   echo "</td></tr>";
 }
 
